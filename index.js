59d60e4a2217b86b73738c007279ffc34065a205
Hassaan Sami <hassaan.sami@live.com>
2017-04-24 19:41:01 -0400

    Handle error for statusCode's other than 200.

--- index.js
+++ index.js
@@ -16,20 +16,24 @@ function printError(e) {
 function getWeather() {
     try {
         const request = https.get(
-            `https://api.wunderground.com/api/${api.key}/conditions/q/${location}.json`, res => {
-                let body = '';
+            `https://api.wunderground.com/api/${api.key}/conditions/q/${location}.json`, (res) => {
+                if (res.statusCode === 200) {
+                    let body = '';
 
-                res.on('data', chunk => {
-                    body += chunk.toString();
-                });
+                    res.on('data', chunk => {
+                        body += chunk.toString();
+                    });
 
-                res.on('end', () => {
-                    //parse body string into json
-                    const weather = JSON.parse(body);
-                    //print relevent data to the console. Wind string starts with 'From', so slice out F from string and print f.
-                    const printString = `Current temperature in ${weather.current_observation.display_location.full} is ${weather.current_observation.temperature_string}, with winds f${weather.current_observation.wind_string.slice(1)}.`;
-                    console.log(printString);
-                });
+                    res.on('end', () => {
+                        //parse body string into json
+                        const weather = JSON.parse(body);
+                        //print relevent data to the console. Wind string starts with 'From', so slice out F from string and print f.
+                        const printString = `Current temperature in ${weather.current_observation.display_location.full} is ${weather.current_observation.temperature_string}, with winds f${weather.current_observation.wind_string.slice(1)}.`;
+                        console.log(printString);
+                    });
+                } else {
+                    const statusCodeError = new Error(`There was an error getting the message for ${readableQuery}. (${http.STATUS_CODES[response.statusCode]})`);
+                }
             }
         );
     } catch (e) {
