61fdfab6f47a25ec993318718dfdc89293d77841
Hassaan Sami <hassaan.sami@live.com>
2017-04-24 19:25:08 -0400

    Put get req in fcn, and create error print fcn.

--- index.js
+++ index.js
@@ -7,25 +7,31 @@ const api = require('./key.json');
 const location = process.argv.slice(2).join('_').replace(' ', '_');
 const readableLoc = location.replace('_', ' ');
 
-//make a get request to the API with your key and city
-const request = https.get(
-    `https://api.wunderground.com/api/${api.key}/conditions/q/${location}.json`, res => {
-        let body = '';
+//function to print error message
+function printError(e) {
+    console.error(e.message);
+}
 
-        res.on('data', chunk => {
-            body += chunk.toString();
-        });
+//function to make a get request to the API with your key and city
+function getWeather() {
+    const request = https.get(
+        `https://api.wunderground.com/api/${api.key}/conditions/q/${location}.json`, res => {
+            let body = '';
 
-        res.on('end', () => {
-            //parse body string into json
-            const weather = JSON.parse(body);
-            //print relevent data to the console. Wind string starts with 'From', so slice out F from string and print f.
-            const printString = `Current temperature in ${weather.current_observation.display_location.full} is ${weather.current_observation.temperature_string}, with winds f${weather.current_observation.wind_string.slice(1)}.`;
-            console.log(printString);
-        });
-    }
-);
+            res.on('data', chunk => {
+                body += chunk.toString();
+            });
 
+            res.on('end', () => {
+                //parse body string into json
+                const weather = JSON.parse(body);
+                //print relevent data to the console. Wind string starts with 'From', so slice out F from string and print f.
+                const printString = `Current temperature in ${weather.current_observation.display_location.full} is ${weather.current_observation.temperature_string}, with winds f${weather.current_observation.wind_string.slice(1)}.`;
+                console.log(printString);
+            });
+        }
+    );
+}
 
 //handle any errors
 
