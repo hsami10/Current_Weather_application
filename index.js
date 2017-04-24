9aa7bea163292105e3211ccc55d1cfb23c8d74e7
Hassaan Sami <hassaan.sami@live.com>
2017-04-24 18:17:04 -0400

    Add https get request and data/end events.

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ const location = process.argv.slice(2).join('_').replace(' ', '_');
 const readableLoc = location.replace('_', ' ');
 
 //make a get request to the API with your key and city
+const request = https.get(
+    `http://api.wunderground.com/api/${api.key}/conditions/q/${location}.json`, res => {
+        let body = '';
+
+        res.on('data', chunk => console.log(chunk));
+
+        res.on('end', () => {
+            console.log(body);
+        });
+    }
+);
 
 //call a print function to print out the information to the console.
 //handle any errors
