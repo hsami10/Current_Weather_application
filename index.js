3303c0f5750b8e35ca47975d0e3229b0d5e00e46
Hassaan Sami <hassaan.sami@live.com>
2017-04-24 18:06:06 -0400

    Get location entered in console and make it a query.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 //require all necessary files
 const https = require('https');
 const http = require('http');
+const api = require('./key.json');
 
-//get the city argument from the console
-//tailor the argument to match the required string format for API
+//get the city argument from the console and make it into a query
+const location = process.argv.slice(2).join('_').replace(' ', '_');
+const readableLoc = location.replace('_', ' ');
+
 //make a get request to the API with your key and city
+
 //call a print function to print out the information to the console.
 //handle any errors
 
