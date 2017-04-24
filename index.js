fe3717c76e2e9ff50b59a23e4015a1891084667f
Hassaan Sami <hassaan.sami@live.com>
2017-04-24 19:49:36 -0400

    Check if weather.location exists. Meaning is the Response for a specific location, or not.

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ const api = require('./key.json');
 
 //get the city argument from the console and make it into a query
 const location = process.argv.slice(2).join('_').replace(' ', '_');
-const readableLoc = location.replace('_', ' ');
+const readableLoc = location.replace('_', ' '); //create readable location string for error messages.
 
 //function to print error message
 function printError(e) {
     console.error(e.message);
+}
+
+//function to print weather in sentence form
+function printWeather(weather) {
+    const printString = `Current temperature in ${weather.current_observation.display_location.full} is ${weather.current_observation.temperature_string}, with winds f${weather.current_observation.wind_string.slice(1)}.`;
+    console.log(printString);
 }
 
 //function to make a get request to the API with your key and city
@@ -28,11 +34,17 @@ function getWeather() {
                         //parse body string into json
                         const weather = JSON.parse(body);
                         //print relevent data to the console. Wind string starts with 'From', so slice out F from string and print f.
-                        const printString = `Current temperature in ${weather.current_observation.display_location.full} is ${weather.current_observation.temperature_string}, with winds f${weather.current_observation.wind_string.slice(1)}.`;
-                        console.log(printString);
+                        if (weather.location) {
+                            printWeather(weather);
+                        } else {
+                            const locNotFound = new Error(`The location "${readableLoc}" was not found.`);
+                            printError(locNotFound);
+                        }
                     });
                 } else {
-                    const statusCodeError = new Error(`There was an error getting the message for ${readableQuery}. (${http.STATUS_CODES[response.statusCode]})`);
+                    //create human readable error message for status code. 
+                    const statusCodeError = new Error(`There was an error getting the message for ${readableLoc}. (${http.STATUS_CODES[response.statusCode]})`);
+                    printError(statusCodeError);
                 }
             }
         );
