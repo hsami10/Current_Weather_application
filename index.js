31c079e963783146864fc7d7be4ecc85d4a04bad
Hassaan Sami <hassaan.sami@live.com>
2017-04-24 16:45:14 -0400

    Write a plan (in comments)

---
+++ index.js
@@ -0,0 +1,9 @@
+//require all necessary files
+
+
+//get the city argument from the console
+//tailor the argument to match the required string format for API
+//make a get request to the API with your key and city
+//call a print function to print out the information to the console.
+//handle any errors
+
