3f543f3a927240f299c92d4842330bab6fe65d16
Hassaan Sami <hassaan.sami@live.com>
2017-04-24 19:28:36 -0400

    Add try-catch block to get method.

--- index.js
+++ index.js
@@ -14,24 +14,30 @@ function printError(e) {
 
 //function to make a get request to the API with your key and city
 function getWeather() {
-    const request = https.get(
-        `https://api.wunderground.com/api/${api.key}/conditions/q/${location}.json`, res => {
-            let body = '';
+    try {
+        const request = https.get(
+            `https://api.wunderground.com/api/${api.key}/conditions/q/${location}.json`, res => {
+                let body = '';
 
-            res.on('data', chunk => {
-                body += chunk.toString();
-            });
+                res.on('data', chunk => {
+                    body += chunk.toString();
+                });
 
-            res.on('end', () => {
-                //parse body string into json
-                const weather = JSON.parse(body);
-                //print relevent data to the console. Wind string starts with 'From', so slice out F from string and print f.
-                const printString = `Current temperature in ${weather.current_observation.display_location.full} is ${weather.current_observation.temperature_string}, with winds f${weather.current_observation.wind_string.slice(1)}.`;
-                console.log(printString);
-            });
-        }
-    );
+                res.on('end', () => {
+                    //parse body string into json
+                    const weather = JSON.parse(body);
+                    //print relevent data to the console. Wind string starts with 'From', so slice out F from string and print f.
+                    const printString = `Current temperature in ${weather.current_observation.display_location.full} is ${weather.current_observation.temperature_string}, with winds f${weather.current_observation.wind_string.slice(1)}.`;
+                    console.log(printString);
+                });
+            }
+        );
+    } catch (e) {
+        printError(e);
+    }
 }
+
+getWeather();
 
 //handle any errors
 
