f321ff629d7b656a0203fa945bf317594f79c407
Hassaan Sami <hassaan.sami@live.com>
2017-04-29 15:10:44 -0400

    Fix module.exports object in weather.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const getWeather = require('./weather.js');
+const weather = require('./weather.js');
 
 //get the city argument from the console and make it into a query
 const location = process.argv.slice(2).join('_').replace(' ', '_');
-getWeather(location);
+//call the getWeather function in weather.js to get the weather at location
+weather.getWeather(location);
 
--- weather.js
+++ weather.js
@@ -57,4 +57,4 @@ function getWeather(location) {
     }
 }
 
-module.exports = getWeather;
+module.exports.getWeather = getWeather;
